211ef20eceacdd20e4a3b89d450eb7bffe0134e6
Christina Nguyen <cnguyen4290@yahoo.com>
2021-08-21 01:04:32 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+
+function superbowlWin(array) {
+    let winningYear = array.find((array) => array.result === "W");
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+        return undefined;
+    }
+  }
+  superbowlWin(record);
+  
+
+
